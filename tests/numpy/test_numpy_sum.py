e78be9a2d36f7e8484fd89cbd6f1aee4e5a295ed
Matthew Blackledge <mattyblackledge@gmail.com>
2024-08-28 09:42:19 +0100

    Improved tests

--- tests/numpy/test_numpy_sum.py
+++ tests/numpy/test_numpy_sum.py
@@ -11,16 +11,16 @@ def local_data():
 
 
 def test_numpy_sum_shared(shared_data):
-    assert pytest.approx(np.sum(shared_data), 15, abs=1e-3)
+    assert np.sum(shared_data) == pytest.approx(15, abs=1e-3)
 
 
 def test_numpy_cumsum_shared(shared_data):
-    assert pytest.approx(np.cumsum(shared_data), [1, 3, 6, 10, 15], abs=1e-3)
+    assert np.cumsum(shared_data) == pytest.approx([1, 3, 6, 10, 15], abs=1e-3)
 
 
 def test_numpy_sum_local(local_data):
-    assert pytest.approx(np.sum(local_data), 0, abs=1e-3)
+    assert np.sum(local_data) == pytest.approx(0, abs=1e-3)
 
 
 def test_numpy_cumsum_local(local_data):
-    assert pytest.approx(np.cumsum(local_data), [-2, -3, -3, -2, 0], abs=1e-3)
+    assert np.cumsum(local_data) == pytest.approx([-2, -3, -3, -2, 0], abs=1e-3)
